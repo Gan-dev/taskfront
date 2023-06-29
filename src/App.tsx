0a3d767a34f7ccbc5c52de1608234076a14e3bfb
Gan-Dev <60898077+Gan-dev@users.noreply.github.com>
2023-06-29 11:03:42 +0200

    DONE

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { TaskProvider } from "./contexts/TaskContext"
 const App = () => {
   return (
     <div className="bg-zinc-900 h-screen text-white flex items-center justify-center">
-      <div className="bg-gray-950 p-4 w-2/5">
+      <div className="bg-gray-950 p-4 md:w-2/5">
         <h1 className="text-3xl fint-bold text-center block my-2">Task app</h1>
         <TaskProvider>
           <TaskForm />
